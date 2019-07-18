4a1410dadd5a41e91cac4247da626ffa4e2b994a
sungjune.p <egs060@gmail.com>
2019-07-18 18:40:57 +0900

    commit gpu_nms

--- lib/fast_rcnn/nms_wrapper.py
+++ lib/fast_rcnn/nms_wrapper.py
@@ -6,7 +6,7 @@
 # ----------------------------------------------------------
 
 from fast_rcnn.config import cfg
-from nms.gpu_nms import gpu_nms
+# from nms.gpu_nms import gpu_nms
 from nms.cpu_nms import cpu_nms, cpu_soft_nms
 import numpy as np
 
